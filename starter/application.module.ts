17d8f710e84cb1f8ff0134fe4546202dd310d005
twilroad <269044570@qq.com>
2018-05-28 11:33:33 +0800

    refactor(*): reset structures

---
+++ starter/application.module.ts
@@ -0,0 +1,31 @@
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { GraphQLFactory, GraphQLModule } from "@nestjs/graphql";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { graphiqlExpress, graphqlExpress } from "apollo-server-express";
+
+import { SmsModule } from "../src/modules/sms.module";
+
+@Module({
+    imports: [
+        GraphQLModule,
+        TypeOrmModule.forRoot(),
+        SmsModule,
+    ],
+})
+export class ApplicationModule implements NestModule {
+    constructor(private readonly graphQLFactory: GraphQLFactory) {
+    }
+
+    configure(consumer: MiddlewareConsumer) {
+        const typeDefs = this.graphQLFactory.mergeTypesByPaths("./**/*.graphql");
+        const schema = this.graphQLFactory.createSchema({ typeDefs });
+
+        consumer
+            .apply(graphiqlExpress({ endpointURL: "/graphql" }))
+            .forRoutes("/graphiql")
+            .apply(graphqlExpress(req => (
+                { schema, rootValue: req }
+            )))
+            .forRoutes("/graphql");
+    }
+}
