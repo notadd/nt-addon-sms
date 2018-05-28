4ca6b1031547f35da2c1e3eae8ce432328b235ad
twilroad <269044570@qq.com>
2018-05-28 11:47:18 +0800

    chore(*): add package.json

---
+++ package/index.d.ts
@@ -0,0 +1,1 @@
+export { SmsModule } from "./sms.module";
---
+++ package/index.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var sms_module_1 = require("./sms.module");
+exports.SmsModule = sms_module_1.SmsModule;
+
+//# sourceMappingURL=index.js.map
