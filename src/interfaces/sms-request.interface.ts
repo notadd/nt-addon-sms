f795792e29c3e53cd8dc889277d89ab4fb6168d2
bingoyao <insistyao_0522@163.com>
2018-06-12 15:36:24 +0800

    chore(interface): change some property to optional

--- src/interfaces/sms-request.interface.ts
+++ src/interfaces/sms-request.interface.ts
@@ -1,13 +1,16 @@
 export interface SmsRequest {
     appId: string;
 
-    appKey: string;
+    // appkey 是由service自动从数据库拉取的，发短信时不需要传入
+    appKey?: string;
 
-    signName: string;
+    // signName 是由service自动从数据库拉取的，发短信时不需要传入
+    signName?: string;
 
     templateId: number;
 
-    templateParam: Array<string>;
+    // templateParam 是由service生成的验证码和有效期，发短信时不需要传入
+    templateParam?: Array<string>;
 
     mobile: Array<string>;
 }
